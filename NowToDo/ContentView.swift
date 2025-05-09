795978f6f6f9368b9b9d0feb01144821bafdaf73
sangsooLee <ss_127@naver.com>
2025-05-09 17:43:06 +0900

    Initial Commit

---
+++ NowToDo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NowToDo
+//
+//  Created by 이상수 on 5/9/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
